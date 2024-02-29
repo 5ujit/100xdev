3d906d17ae2186050222ad3c4eb2c4fad1d2f9e4
5ujit <149666757+5ujit@users.noreply.github.com>
2024-02-29 19:53:52 +0530

    first week

---
+++ Week 1.2/02loops.js
@@ -0,0 +1,9 @@
+// for (let i=0;i<=1000; i++){
+//   console.log(i);
+// }
+
+let ans =0;
+for(let i=1; i<=100; i++){
+  ans=ans+i;
+}
+console.log(ans);
---
+++ Week 1.2/07function.js
@@ -0,0 +1,10 @@
+function findSum(a,b){
+  return a+ b;
+}
+// findSum(3,4)
+
+const value = findSum(1,3);
+const value1 = findSum(1,30);
+
+console.log(value);
+console.log(value1);
