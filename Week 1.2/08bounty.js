0c970fdd7476d2e50de0f0e0ab7f404300b1b738
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-01 23:39:57 +0530

    function

--- Week 1.2/08bounty.js
+++ Week 1.2/08bounty.js
@@ -1,5 +1,5 @@
 let sum = 0;
-for (let i = 0; i < 10000000000; i++) {
+for (let i = 0; i < 100; i++) {
   sum = sum + i;
 }
 console.log(sum);
---
+++ Week 1.2/09SeTimeout.js
@@ -0,0 +1,23 @@
+// 👇👇👇👇👇👇 setTime function👇👇👇👇👇👇👇
+
+// function greet(){
+//   console.log("Hello world");
+// }
+
+// setTimeout(greet, 5 *1000) 
+
+
+
+// the promet will print after 5 second
+ // this is also  call back function 
+
+ 
+
+//                         👇👇👇👇👇👇setInterval function👇👇👇👇👇👇👇
+//
+
+function greet(){
+  console.log("hello world");
+
+}
+setInterval(greet, 5 *1000)
