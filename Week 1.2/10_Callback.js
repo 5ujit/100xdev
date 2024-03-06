1eaa55c5e73ad412b79121cb41094f752bb47d4d
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-06 13:03:55 +0530

    call back

---
+++ Week 1.2/10_Callback.js
@@ -0,0 +1,17 @@
+// white another function that display this risult in a pretty format no
+function sum(num1, num2){
+  let result  = num1 + num2;
+  return result
+}
+console.log(sum(1,5));
+
+function displayResult(data){
+  console.log("Result of sum is :" +data);
+
+}
+function displayResultPass(ok){
+  console.log("Sum result :"+ ok );
+}
+
+const ans = sum(1,2);
+displayResultPass(ans);
