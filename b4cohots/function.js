fbb2e6699f37b663ad7e5d163d5c24f09e0199a8
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-22 19:13:25 +0530

    basics

---
+++ b4cohots/function.js
@@ -0,0 +1,19 @@
+function singHappyBirthday(){
+  console.log("happy birthday to you ....");
+}
+
+
+
+// call this function now 
+// call || invoke|| run all are same 
+function twoPlusFour(){
+  console.log(2+4);
+}
+twoPlusFour();
+
+
+function add(){
+  console.log( "2nd fun 👉",4+5);
+}
+add();
+
