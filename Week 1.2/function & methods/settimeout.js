529fcf6ce27f4fb0755c90b7145024802b8a963a
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-22 19:14:36 +0530

    function

---
+++ Week 1.2/function & methods/settimeout.js
@@ -0,0 +1,25 @@
+
+// function greet(){
+//   console.log("hello world");
+
+// }
+// setInterval(greet, 5 *1000)
+
+
+
+
+
+
+
+
+
+
+function greet(){
+  let a = "welcome baba ";
+  console.log(a);
+}
+
+
+
+setInterval(greet, 2000);
+
