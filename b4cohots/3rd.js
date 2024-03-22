95b7df7cacddee2848a8731c9e48eaac857390e3
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-22 19:13:14 +0530

    basics

---
+++ b4cohots/3rd.js
@@ -0,0 +1,26 @@
+
+// function
+/*
+now we use the function what ever we give 
+it will give return value 
+like sum of 2 no 
+when ever i give 23,34 or 3,4
+it will operate for me 
+*/
+
+function singHappyBirthday(){
+  console.log("happy birthday to you ....");
+}
+
+
+function sumThreeNo(number1 ,number2,number3){ // parameter
+  return number1 +number2+ number3;
+
+}
+
+
+const Value = sumThreeNo(2,3,4);
+console.log(Value);
+
+
+
