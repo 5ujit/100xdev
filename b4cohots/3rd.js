92bdea3ff7b925775fec93b0862736e18bfbb803
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-22 22:05:29 +0530

    function

--- b4cohots/3rd.js
+++ b4cohots/3rd.js
@@ -5,22 +5,22 @@ now we use the function what ever we give
 it will give return value 
 like sum of 2 no 
 when ever i give 23,34 or 3,4
-it will operate for me 
+it will opratate for me 
 */
-
 function singHappyBirthday(){
   console.log("happy birthday to you ....");
 }
 
 
-function sumThreeNo(number1 ,number2,number3){ // parameter
-  return number1 +number2+ number3;
+function sumTwoNo(a,b){ // parameter
+  c= a+b;
+  
 
 }
 
 
-const Value = sumThreeNo(2,3,4);
+const Value = sumTwoNo();
 console.log(Value);
-
+console.log(undefined +undefined);
 
 
