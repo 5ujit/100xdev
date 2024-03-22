ea285697c94657f735da4b517288c6d6c393d120
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-22 19:13:01 +0530

    basics

---
+++ b4cohots/2nd.js
@@ -0,0 +1,21 @@
+function singHappyBirthday(){
+  console.log("happy birthday to you ....");
+}
+
+/*
+in this program we put the whole function in one variable  
+then call that particular variable 
+*/
+
+
+function twoPlusFour(){
+  return 2+4;
+
+}
+
+
+const Value = twoPlusFour();
+console.log(Value);
+
+
+
