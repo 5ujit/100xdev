a05f62a47db6fead2830bfb2b6d5914a7bcae1d1
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-23 23:35:31 +0530

    hosting

---
+++ b4cohots/7Hoasting.js
@@ -0,0 +1,15 @@
+ // hosting
+
+// hello();
+
+// function hello(){
+//   console.log("......hello world.....");
+// }
+//                      calling b4 making the variabale
+// console.log(hello);
+// const hello= "hello world ";
+
+ // after making the variable
+
+// console.log(hello);
+
