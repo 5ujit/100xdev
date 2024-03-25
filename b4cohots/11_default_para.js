eece93b392e5efd5f79107a1d2102bf9b5d18247
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-25 11:52:48 +0530

    default patameter

---
+++ b4cohots/11_default_para.js
@@ -0,0 +1,22 @@
+// default parameters
+
+/*
+function addTwo(a,b){
+  if(typeof b=== "undefined"){
+    b=1;
+  }
+  return a+b;
+}
+
+*/
+
+
+function addTwo(a,b){
+ 
+  return a+b;
+}
+
+
+
+const ans = addTwo(3,5);
+console.log(ans);
