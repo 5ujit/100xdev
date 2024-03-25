47b8a17fdc14070186a908058ca9efa92e31078d
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-25 18:53:17 +0530

    setTimeout

---
+++ b4cohots/setTimeout.js
@@ -0,0 +1,18 @@
+ setTimeout(function() {console.log("dfasdfas");}, 3000); 
+ // 3000 milliseconds = 3 seconds
+
+
+
+
+
+
+const a=()=>{
+  console.log("appleMango");
+}
+setTimeout(a,3000);
+
+
+
+setTimeout(function() {
+  console.log("appleMango");
+}, 3000);
