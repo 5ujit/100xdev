4461fe9f7af047842c2b209ba9ab2ee103d144c8
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-27 19:29:15 +0530

    simple function add two no

---
+++ Week 1.2/14.js
@@ -0,0 +1,31 @@
+/*
+function 
+ 1.Abstract the logic in your program
+ 2. Takes arguments as an inputs
+ 3. Return a value as an output
+ 4. You can think of them as an independent program that is supposed  to do something
+ given an input 
+ 5. Function Can takes other function as input this is improving me as a logics 
+
+*/
+
+// lets write some code 
+/*
+Write a function that finds sum of two numbers
+write another function that displays this results in preety format
+write another function that takes this sum and prints it in passive tense
+*/
+
+findSum =(a,b)=>{
+  const c = a+b;
+  return c;
+}
+const add= findSum(2,4);
+console.log(add);
+
+
+
+// function sumValue(a,b){
+
+// }
+// const sumValue = a+b;
