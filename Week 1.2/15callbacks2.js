246442b0eca8b67818260fcc2272107158c4ac91
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-28 20:30:42 +0530

    callbacks

---
+++ Week 1.2/15callbacks2.js
@@ -0,0 +1,28 @@
+/*
+const sum=(num1, num2)=>{
+  let result = num1 +num2;
+  return result;
+}
+
+
+const displayResult =(data)=>{
+  console.log("Results of the sum is : "+ data);
+}
+
+const displayResultpass=(data)=>{
+  console.log("Sum results is : "+ data);
+}
+
+ 
+const ans =  sum(1,2);{
+  // displayResultpass(ans);
+  console.log(ans);
+}
+
+*/
+const calculateArithmetic=(a,bc, c)=>{
+  const ans = c(a,b)
+  return ans;
+
+}
+displayResult(sum(1,2))
