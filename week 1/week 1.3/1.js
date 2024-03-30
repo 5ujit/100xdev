189416e27cddadd7e30a51b6ea0e3d2a8b0eb4dc
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-30 19:06:29 +0530

    week 1.3

---
+++ week 1/week 1.3/1.js
@@ -0,0 +1,14 @@
+function getLength(str){
+  console.log("Original String:", str);
+  console.log("length :", str.length);
+}
+getLength("Hello World");
+
+console.log("===========---------------============");
+
+let apple=(a)=>{
+  console.log("this is first string :", a);
+  console.log("length of the string : ", a,length );
+
+}
+getLength("apple mango");
