84f0176f73c5fe59757afa9cdccea4e8e8410c26
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-31 18:03:43 +0530

    slice

---
+++ week 1/week 1.3/2.js
@@ -0,0 +1,30 @@
+// slice 
+
+// function getSlice(str, star,end){
+//   console.log("Original String: ",str);
+//   console.log("After slice:", str.slice(star,end));
+// }
+// getSlice("Hello World",0,5);
+
+
+// const value = "harkirat"
+
+// let ans = value.slice(0,5)
+// console.log(ans)
+
+
+// const value = "harkirat"
+
+// let ans = value.substr(0,5)
+// console.log(ans)
+
+
+
+const value = "abcdefghij";
+let ans = value.substr(0,5);//
+let ans2 = value.slice(0,6);
+console.log("this is substr :", ans);
+console.log("this is slice ok :",ans2);
+
+
+
