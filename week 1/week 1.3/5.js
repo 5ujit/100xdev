1dbd974f3f6c0540056fa7eb05a4d46c85942ab8
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-31 18:04:12 +0530

    trim

---
+++ week 1/week 1.3/5.js
@@ -0,0 +1,11 @@
+// trim 
+// const value = "    sujit srivastav    "
+// console.log(value.trim(""));  
+
+
+const  trimString=(str)=>{
+    console.log("Orignal string:" ,str);
+    console.log("After the trimed the string :",str.trim());
+
+}
+trimString("            hello world universe duniya                 ");
