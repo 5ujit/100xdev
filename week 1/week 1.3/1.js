0ae9c1523b4a0cec3c6b6cc1743da0bb27450049
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-31 18:03:21 +0530

    base strong

--- week 1/week 1.3/1.js
+++ week 1/week 1.3/1.js
@@ -1,10 +1,42 @@
-function getLength(str){
-  console.log("Original String:", str);
-  console.log("length :", str.length);
-}
-getLength("Hello World");
 
-console.log("===========---------------============");
+// function getLength(str){
+//   console.log("Original String:", str);
+//   console.log("length :", str.length);
+// }
+// getLength("Hello World");
+
+// console.log("===========---------------============");
+
+
+
+// const getName=(fruits)=>{
+//   console.log("Original Strings: ", fruits);
+//   console.log("length of this string: ", fruits.length);
+//   getName("this is length ")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 let apple=(a)=>{
   console.log("this is first string :", a);
@@ -12,3 +44,70 @@ let apple=(a)=>{
 
 }
 getLength("apple mango");
+
+
+function sum(a,b){
+  return a+b;
+}
+const finalSum = sum(3,4)
+
+  console.log(finalSum);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function fundIndexOf(str, target ){
+  console.log("Original Strings:", str);
+  console.log("Index:",str.indexOf(target));
+}
+fundIndexOf("Hello world","world");
+*/
+
+//code 2
+
+/*
+const findIndexof=(abc,point )=>{
+  console.log("this is the string i have written: ",abc );
+  console.log("This is index :",abc.indexOf(point));
+
+
+}
+findIndexof("apple mango pineapple", "mango");
+
+*/
+console.log("👉👉👉👉👉👉👉👉 what if you what to target the last word 👈👈👈👈 "); 
+const findIndexof=(abc,point )=>{
+  console.log("this is the string i have written: ",abc );
+  console.log("This is index :",abc.lastIndexOf(point));
+
+
+}
+findIndexof("apple mango  mango mango pineapple", "mango");
+
+
+
+// length lastIndexOf , indexof
+//  in length u use str.length
+//  lastIndexOf()
+//  indexOf()
