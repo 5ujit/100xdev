a5109f639e82bea8baf90c53c5f4544c81de6091
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-31 18:05:13 +0530

    toUpper & toLower cace

---
+++ week 1/week 1.3/6.js
@@ -0,0 +1,8 @@
+// toUpper & toLower cace 
+
+const value = "    sujit srivastav    "
+console.log(value.toUpperCase());  
+
+
+const value2 = "    SUJIT SRIVASTAV    "
+console.log(value2.toLowerCase());  
