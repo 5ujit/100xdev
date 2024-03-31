b99f2bdca7eed835f418ffacb36dd9d4bfe5aeea
5ujit <149666757+5ujit@users.noreply.github.com>
2024-03-31 18:04:58 +0530

    replace

---
+++ week 1/week 1.3/3.js
@@ -0,0 +1,30 @@
+/*
+let originalString = "apple, banana, orange, apple";
+let newString = originalString.replace("apple", "pear");
+console.log(newString); // Output: pear, banana, orange, apple
+
+// Now, if we use /g:
+// With /g, all occurrences of "apple" are replaced with "pear".
+
+
+let originalString = "apple, banana, orange, apple";
+let newString = originalString.replace(/apple/g, "pear");
+console.log(newString); // Output: pear, banana, orange, pear
+
+let originalString = "apple , banana, orange , apple, pie, apple";
+let newString = originalString.replace(/apple/g, "iron man" );
+console.log(newString);
+
+
+
+let originalString = "apple , banana , orange , apple";
+let newString= originalString.replace("apple", "iron man" );
+console.log(newString); 
+*/
+
+// let originalString = "apple , banana , orange , apple";
+// let newString= originalString.replace ("apple", "iron man" );
+// console.log(newString); 
+
+const str = "hello worlfsgsd";
+console.log(str.replace("he", "javascript"));
