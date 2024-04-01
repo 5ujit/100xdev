dc5117a6b5da3984432a773b74880f17ce7341cb
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-01 23:29:48 +0530

    numbers

---
+++ week 1/week 1.3/7number.js
@@ -0,0 +1,21 @@
+/*
+function explainParseInt(value){
+  console.log("Orignal Value: ", value);
+  
+  let result = parseInt(value);
+  console.log("After the Parse int :",result);
+}
+
+explainParseInt("42"); 
+
+explainParseInt("3.14"); 
+
+explainParseInt("Hello");
+
+console.log(parseInt("42")); 
+console.log(parseInt("3.14"));
+console.log(parseInt("99ajad "));
+*/
+console.log(parseFloat("42.99000")); 
+console.log(parseFloat("3.14"));
+console.log(parseFloat("99ajad "));
