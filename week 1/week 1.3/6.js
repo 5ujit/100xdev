9fee68a624f93d631c1ae33e837a1df4a5f817d4
5ujit <149666757+5ujit@users.noreply.github.com>
2024-04-03 22:25:51 +0530

    Update 6.js

--- week 1/week 1.3/6.js
+++ week 1/week 1.3/6.js
@@ -1,8 +1,8 @@
 // toUpper & toLower cace 
 
-const value = "    sujit srivastav    "
+const value = "    sujit srivastav    ";
 console.log(value.toUpperCase());  
 
 
-const value2 = "    SUJIT SRIVASTAV    "
+const value2 = "    SUJIT SRIVASTAV    ";
 console.log(value2.toLowerCase());  
