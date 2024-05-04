73187d9e6f9a0974e8e8763fb2900dd5dbf2b3e1
5ujit <149666757+5ujit@users.noreply.github.com>
2024-05-04 23:14:22 +0530

    Update index.js

--- week 1/week1.5/index.js
+++ week 1/week1.5/index.js
@@ -3,5 +3,11 @@
 
 // var d2= new Dome();
 // console.log(d2.getDate());
+var d =new Promise(function(resolve){
+  setTimeout(function(){
+    resolve("foo")  ;
+  },1000)
 
-v
+});
+console.log(d);
+
