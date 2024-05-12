988e92b551156c7a4610d425e08f3037bd15f836
5ujit <149666757+5ujit@users.noreply.github.com>
2024-05-12 23:41:55 +0530

    Create index.js

---
+++ week 2/week 2.2/http server/index.js
@@ -0,0 +1,9 @@
+co
+//create a todo that todo list users store todos on the server
+
+/*
+  create  a server a http server from scratch in c
+  try in http server rust using actix-web
+  create an http server in golang using gurrila framework
+  spring boot java 
+*/
