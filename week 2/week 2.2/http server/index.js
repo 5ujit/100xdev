793d08d49aa0386cbb8ee6c3722ada6efbadc786
5ujit <149666757+5ujit@users.noreply.github.com>
2024-05-14 03:10:07 +0530

    Update index.js

--- week 2/week 2.2/http server/index.js
+++ week 2/week 2.2/http server/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require("express")
 
 const app = express();
 
 const port = 3000
 
 app.get('/',function(req,res){
-  res.send("hello ji  !!!!");
+  res.send("hello ji baba !!!!");
 })
 app.listen
 
