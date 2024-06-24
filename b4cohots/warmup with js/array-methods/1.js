3fa74b7eec08465d3348616775b66437e7fce6d5
5ujit <149666757+5ujit@users.noreply.github.com>
2024-06-25 01:15:32 +0530

    Create 1.js

---
+++ b4cohots/warmup with js/array-methods/1.js
@@ -0,0 +1,76 @@
+// inportant array methods
+
+
+// const array = [4,2,3,4,5]; // this is array 
+
+// function myfun(number,index){// this is function
+ 
+    // console.log(`index is,${index} number is ${number}`);    
+    
+// }
+// numbers.forEach(function(number){
+//     console.log(number*3);
+// })
+
+
+
+
+
+
+
+// for (let i= 0; i<array.length; i++){
+    
+//     myfun(array[i],i)
+// }
+
+
+
+
+// const ans = multiplyBy2(22,1);
+// console.log(ans);
+
+
+
+
+
+
+
+   // console.log(`index is,${index} number is${number}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   const users=[
+    {firstName: "harshit", age: 23},
+    {firstName: "mhoit", age:21 },
+    {firstName: "nitish", age: 20},
+    {firstName: "garima", age: 20}
+   ]
+
+   users.forEach(function(user){
+    console.log(user.firstName, user.age);
+   })
