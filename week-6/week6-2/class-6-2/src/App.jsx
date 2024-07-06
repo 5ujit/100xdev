31923e222849bd45304637e4f65e72355d4b4747
5ujit <149666757+5ujit@users.noreply.github.com>
2024-07-06 21:02:03 +0530

    Update App.jsx

--- week-6/week6-2/class-6-2/src/App.jsx
+++ week-6/week6-2/class-6-2/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  const calculateSum = (num) => {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+      sum += i;
+    }
+    return sum;
+  };
+
+  const sum = calculateSum(inputValue);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input
+        type="number"
+        onChange={(e) => setInputValue(Number(e.target.value))}
+        placeholder="Enter a number"
+      />
+      <br />
+      <h2> Sum of numbers up to {inputValue} is {sum} </h2>
+      <br />
+      <button onClick={() => setCounter(counter + 1)}>
+        Counter ({counter})
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
