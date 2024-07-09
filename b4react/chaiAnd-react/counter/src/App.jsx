6c1ddc80b44bc8e16c6cd21b0683cc549da458ea
5ujit <149666757+5ujit@users.noreply.github.com>
2024-07-09 16:04:59 +0530

    Create App.jsx

---
+++ b4react/chaiAnd-react/counter/src/App.jsx
@@ -0,0 +1,42 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useState } from 'react';
+
+
+function App() {
+  let [counter,setCounter]=useState(15)
+  
+  // let counter= 15;
+  const addValue=()=>{    
+    console.log("clicked ",counter);
+    if(counter <=20){
+      // counter =counter +1
+      setCounter(counter +1)
+    }
+   
+
+  }
+  const removeValue= ()=>{
+    if(counter >0){
+    setCounter(counter - 1)
+  }
+
+  }
+
+  return (
+    <>
+    <h1> hello Aliens </h1>
+    <h2>Counter value {counter} </h2>
+
+    <button
+    onClick={addValue}
+    >Add value {counter} </button>
+    <br />
+
+    <button onClick={removeValue}>remove value {counter} </button>
+    </>
+  )
+}
+
+export default App
