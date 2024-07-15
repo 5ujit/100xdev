71f4d062e3422b0b688fac701e11724dc7c7c0aa
5ujit <149666757+5ujit@users.noreply.github.com>
2024-07-16 02:09:30 +0530

    useRef at last 6.3L

--- week-6/week6-3/Class6-3/recat-app-test/src/App.jsx
+++ week-6/week6-3/Class6-3/recat-app-test/src/App.jsx
@@ -3,18 +3,20 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 
 function App() {
+  const [incomeTex, setIncomeTex]= useState(20000);
 
+  const divRef=useRef();
   useEffect(() => {
     setTimeout(() => {
-      document.getElementById("incomeTextContainer").innerHTML=10
+      divRef.current.innerHTML=10
     }, 5000);
   }, [])
 
-  const incomeTax = 20000;
+  // const incomeTax = 20000;
 
   return (
     <div>
-        hi there, your income tax returns are <div id="incomeTextContainer">{incomeTax}</div>
+        hi there, your income tax returns are <div ref={divRef} >  {incomeTex} </div>
     </div>
   )
 }
