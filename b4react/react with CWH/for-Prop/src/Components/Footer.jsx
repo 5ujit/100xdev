63c813f6f496603f740df48da5ec5a81128bc0ef
5ujit <149666757+5ujit@users.noreply.github.com>
2024-07-26 23:12:12 +0530

    Create Footer.jsx

---
+++ b4react/react with CWH/for-Prop/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import  "./Footer.css"
+const Footer =()=>{
+    return (
+        <div className="footer">
+             Copyrigt &copy; WWWstockmarketnews.com| All rights reserved
+        </div>
+    )
+}
+export default Footer;
