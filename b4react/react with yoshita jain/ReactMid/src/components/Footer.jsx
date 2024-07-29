c69fc583e331d4b80d17558d628b7a3a3bae6fdd
5ujit <149666757+5ujit@users.noreply.github.com>
2024-07-29 15:01:59 +0530

    props revision in react

---
+++ b4react/react with yoshita jain/ReactMid/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+    copyright &copy; www.projecthumanity.com | All rigths reserved
+    </div>
+  )
+}
+
+export default Footer
