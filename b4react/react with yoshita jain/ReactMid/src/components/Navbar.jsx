8b3944173ba5964f85a05af539d4ee1fd6125814
5ujit <149666757+5ujit@users.noreply.github.com>
2024-07-29 15:02:15 +0530

    props revision in react

---
+++ b4react/react with yoshita jain/ReactMid/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Navbar.css" 
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>ABOUT US </li>
+          <li>OUR WORK</li>
+          <li>CAMPAIGNS</li>
+          <li>CONSTACT US</li>
+        </ul>
+      </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
