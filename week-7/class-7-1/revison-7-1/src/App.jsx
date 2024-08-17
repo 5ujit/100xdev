ea5e591584e06c5e191d706e7abd66a1eff26517
5ujit <149666757+5ujit@users.noreply.github.com>
2024-08-17 23:57:10 +0530

    Update App.jsx

--- week-7/class-7-1/revison-7-1/src/App.jsx
+++ week-7/class-7-1/revison-7-1/src/App.jsx
@@ -1,13 +1,20 @@
-import {BrowserRouter,Routes,Route}from 'react-router-dom'
+import {BrowserRouter,Routes,Route, useNavigate, }from 'react-router-dom'
 import './App.css'
 import Dashboard from './Components/Dashboard'
 import Landing from './Components/Landing'
 
 function App() {
+  
+
 
   return (
-    <div style={{background: "black", color: "white"}}>
+
+    <div>
+      
+
+
     <BrowserRouter>
+    <Appbar />
       <Routes>
         <Route path="/dashbord" element={<Dashboard />} />
         <Route path="/" element={<Landing />} />
@@ -16,6 +23,28 @@ function App() {
     </BrowserRouter>
   </div>
      )
+}
+
+function Appbar(){
+  const navigate = useNavigate();
+  return(
+    <div>
+       <div>
+      
+      <button onClick={()=>{
+        navigate("/");
+      }} >Landing Page </button>
+    
+      <button onClick={()=>{
+        navigate("/dashbord");
+      }} >Dashnoard </button>
+    
+    </div>
+    </div>
+   
+
+
+  )
 }
 
 export default App
