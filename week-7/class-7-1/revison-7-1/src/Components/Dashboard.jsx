2ed567d991ae8335f3b5266ea41f90b0405ad10c
5ujit <149666757+5ujit@users.noreply.github.com>
2024-09-06 23:12:53 +0530

    Update Dashboard.jsx

--- week-7/class-7-1/revison-7-1/src/Components/Dashboard.jsx
+++ week-7/class-7-1/revison-7-1/src/Components/Dashboard.jsx
@@ -1,29 +1,29 @@
-export default function Dashboard(){
-  return <div>
-    This  is Dashboard page 
-  </div>
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-// const Dashboard = () => {
-//   return (
-//     <div>
-//       this is dashbord page 
-//     </div>
-//   )
+// export default function Dashboard(){
+//   return <div>
+//     This  is Dashboard page 
+//   </div>
 // }
 
-// export default Dashboard
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      this is dashbord page 
+    </div>
+  )
+}
+
+export default Dashboard
