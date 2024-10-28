fd7e95bde1f7980cd76a1b79fd333efcd38bf68c
5ujit <sksujit021@gmail.com>
2024-10-28 22:45:31 +0530

    project 2 making

---
+++ project-2/src/components/navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div class="logo">
+        <img src="/images/logo.png" alt="do some coding logo"  />
+
+      </div>
+     <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+     </ul>
+      
+    </nav>
+  )
+}
+
+export default Navigation
