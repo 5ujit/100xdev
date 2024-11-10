fed01107d891577063487c4fd9d40060064db8d6
5ujit <sksujit021@gmail.com>
2024-11-10 13:43:19 +0530

    7

--- week-7/week7-2/src/App.jsx
+++ week-7/week7-2/src/App.jsx
@@ -1,30 +1,35 @@
-import { useContext,useState } from "react"
+// App.jsx
+import { useContext, useState } from "react";
 import { CountContext } from "./context";
 
-
 function App() {
-  const [count,setCount]=useState(0);
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-     <CountContext.Provider value={count}>
-      <Count setCount={setCount} />
-     </CountContext.Provider>
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
     </div>
-  )
+  );
 }
 
-function Count({setCount}){
-  return <div>
-    <CountRenderer />
-    <Buttons setCount={setCount}/>
-  </div>
+function Count({ setCount }) {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons setCount={setCount} />
+    </div>
+  );
 }
+function CountRenderer({setCount}){
 
-function CountRenderer() {
   const count = useContext(CountContext);
-  return <div>
-    {count}
-  </div>
+  return <>
+  <b>
+  {count}
+  </b>
+  </>
 }
 
 function Buttons({setCount}){
--- week-7/week7-2/src/context.jsx
+++ week-7/week7-2/src/context.jsx
@@ -1,4 +1,5 @@
+// context.jsx
 import { createContext } from "react";
 
-export const CountContext = createContext(0);
+export const CountContext = createContext({count : 0, setCount: null});
 
