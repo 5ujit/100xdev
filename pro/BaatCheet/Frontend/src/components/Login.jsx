99a167d5eca3bece9f0b16c2c4289fbb97c8af92
5ujit <sksujit021@gmail.com>
2024-11-14 16:05:45 +0530

    react project

---
+++ pro/BaatCheet/Frontend/src/components/Login.jsx
@@ -0,0 +1,42 @@
+// src/components/Login.jsx
+import React, { useState } from 'react';
+import { auth } from '../services/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert('Logged in successfully!');
+    } catch (error) {
+      console.error('Error logging in:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ pro/BaatCheet/Frontend/src/services/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ5W7L0wS1h05i0oYIh8WltfqtIEtj47g",
+  authDomain: "baaatcheet-d7548.firebaseapp.com",
+  projectId: "baaatcheet-d7548",
+  storageBucket: "baaatcheet-d7548.firebasestorage.app",
+  messagingSenderId: "114315663449",
+  appId: "1:114315663449:web:e4ebbd77ffb615799b7948"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
