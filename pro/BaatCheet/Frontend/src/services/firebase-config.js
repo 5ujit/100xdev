c8f968eb5973a120c28c2fd5f525bd7ec6047e9c
5ujit <sksujit021@gmail.com>
2024-11-15 23:51:42 +0530

    chat app

--- pro/BaatCheet/Frontend/src/services/firebase-config.js
+++ pro/BaatCheet/Frontend/src/services/firebase-config.js
@@ -1,3 +1,4 @@
+// src/services/firebase-config.js
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
@@ -11,3 +12,57 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id"
+};
+const app = initializeApp(firebaseConfig);
+*/
