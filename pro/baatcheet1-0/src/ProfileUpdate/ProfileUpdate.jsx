4f3f957461519d30cb39eb808e9b30a34111028b
5ujit <sksujit021@gmail.com>
2024-11-18 23:12:25 +0530

    forntend chat app

--- pro/baatcheet1-0/src/ProfileUpdate/ProfileUpdate.jsx
+++ pro/baatcheet1-0/src/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfileUpdate.css'
+import assets from '../assets/assets'
 const ProfileUpdate = () => {
+  const [image,setImage]=useState(false)
   return (
-    <div>
+    <div className='profile'>
+      <div className="prfile-container">
+        <form >
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg .jpeg'hidden />
+              <img src={image ? URL.createObjectURL(image): assets.avatar_icon} alt="" />
+              Upload Profile image            
+
+          </label>
+          <input type='text' placeholder='Your name' requied/>
+          <textarea placeholder='write porfile bo'></textarea>
+          <button type='submit'>Save</button>
+          
+        </form>
+        <img className='profile-pic' src={ image? URL.createObjectURL(image): assets.avatar_icon} alt="" />
+      </div>
 
       
     </div>
