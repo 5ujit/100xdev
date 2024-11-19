d87cbc23b2d1bbb377ef3d249993c8dc21d57ff5
5ujit <sksujit021@gmail.com>
2024-11-19 23:32:37 +0530

    baaacheet ui ready with react

---
+++ pro/socket/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+io.on('connection', (socket) => {
+    socket.on('sendName', (name) => socket.emit('greeting', `Hello ${name.trim()}`));
+});
+
+server.listen(3000);
