cc6f7535bca40fb6a8ffbf2bc458238188e660da
5ujit <sksujit021@gmail.com>
2025-01-04 23:48:50 +0530

    useRef hook

--- CWH/vid-109-useRef/src/App.jsx
+++ CWH/vid-109-useRef/src/App.jsx
@@ -1,43 +1,44 @@
-import { useEffect, useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React, { useEffect , useRef} from 'react'
+
+// const App = () => {
+//   const inputRef= useRef(null);
+// useEffect(()=>{
+//   inputRef.current.focus();
+// },[])
+//   return (
+//     <div>
+//       <input ref={inputRef} placeholder='type here....' />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+import React, { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const boxRef = useRef(null);
 
-  const a= useRef(0)
+  const moveBox = () => {
+    boxRef.current.style.transform = "translateX(200px)"; // Move box 200px to the right
+    boxRef.current.style.transition = "transform 0.5s ease";
+  };
 
-
-  useEffect(()=>{
-    console.log(`ye render ho raha h ${}`);
-    
-  })
-
-  return ( 
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return (
+    <div>
+      <div
+        ref={boxRef}
+        style={{
+          width: "100px",
+          height: "100px",
+          background: "tomato",
+        }}
+      ></div>
+      <button onClick={moveBox}>Move Box</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
