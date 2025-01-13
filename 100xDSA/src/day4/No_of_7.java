517140658bca6fc8d64e4d54b1c3ca60dcc1e89c
5ujit <sksujit021@gmail.com>
2025-01-13 23:59:43 +0530

    Create No_of_7.java

---
+++ 100xDSA/src/day4/No_of_7.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class No_of_7 {
+    public static void main(String[] args) {
+        System.out.println("****");
+        int n= 1323232354;
+        int count =0;
+        int rem;
+        while(n>0){
+            rem=n%10;
+            if()
+        }
+    }
+}
