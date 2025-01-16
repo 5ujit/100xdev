09e088144b73e5dea25752e699e652d739902d1c
5ujit <sksujit021@gmail.com>
2025-01-16 23:48:26 +0530

    Create First.java

---
+++ 100xDSA/src/vidSwitchCase/First.java
@@ -0,0 +1,21 @@
+package vidSwitchCase;
+
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("switch case ");
+        Scanner sc = new Scanner(System.in);
+
+        String fruit = sc.next();
+        if(fruit.equals("mengo")){
+            System.out.println("king of fruts");
+        } else if (fruit.equals("apple")) {
+            System.out.println("a sweet of ");
+
+        }
+
+
+    }
+
+}
