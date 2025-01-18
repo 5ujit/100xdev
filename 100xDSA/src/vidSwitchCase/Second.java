ef4265643d5d0d6013398e9eeb631d5af4c7fc72
5ujit <sksujit021@gmail.com>
2025-01-18 23:52:06 +0530

    Create Second.java

---
+++ 100xDSA/src/vidSwitchCase/Second.java
@@ -0,0 +1,14 @@
+package vidSwitchCase;
+import java.util.Scanner;
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("hello devloper sujit");
+
+   Scanner sc = new Scanner(System.in);
+   
+
+
+
+
+    }
+}
