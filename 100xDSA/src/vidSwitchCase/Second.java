820d7c762bb8fd33eaa338b4b86b36ebfcfeb4c2
5ujit <sksujit021@gmail.com>
2025-01-19 23:52:35 +0530

    Update Second.java

--- 100xDSA/src/vidSwitchCase/Second.java
+++ 100xDSA/src/vidSwitchCase/Second.java
@@ -5,7 +5,23 @@ public class Second {
         System.out.println("hello devloper sujit");
 
    Scanner sc = new Scanner(System.in);
-   
+   String fruit =sc.next();
+   switch (fruit){
+       case "Mango":
+           System.out.println("King of fruits ");
+           break;
+       case"Apple":
+           System.out.println("A apple a day keep doctor away ");
+           break;
+       case "Orange":
+           System.out.println("Round fruits");
+           break;
+
+           
+
+
+
+   }
 
 
 
