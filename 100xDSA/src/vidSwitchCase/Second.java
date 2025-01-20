93920a328f9d2fe9078ac79d7bfaf85174e5ea76
5ujit <sksujit021@gmail.com>
2025-01-20 23:57:10 +0530

    Update Second.java

--- 100xDSA/src/vidSwitchCase/Second.java
+++ 100xDSA/src/vidSwitchCase/Second.java
@@ -5,31 +5,33 @@ public class Second {
         System.out.println("hello devloper sujit");
 
    Scanner sc = new Scanner(System.in);
-   String fruit =sc.next();
-   switch (fruit){
-       case "Mango":
-           System.out.println("King of fruits ");
-           break;
-       case"Apple":
-           System.out.println("A apple a day keep doctor away ");
-           break;
-       case "Orange":
-           System.out.println("Round fruits");
-           break;
-       case "Grapes":
-           System.out.println("small fruits");
-           break;
-       default:
-           System.out.println("please enter a valid fruits");
+//   String fruit =sc.next();
+//        switch (fruit) {
+//            case "Mango" -> System.out.println("King of fruits ");
+//            case "Apple" -> System.out.println("A apple a day keep doctor away ");
+//            case "Orange" -> System.out.println("Round fruits");
+//            case "Grapes" -> System.out.println("small fruits");
+//            default -> System.out.println("please enter a valid fruits");
 
-           
-
-
-
-   }
+        int day = sc.nextInt();
+        switch (day){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wenday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("enter valid day no ");
 
 
 
+
+
+
+
+
+        }
 
     }
 }
