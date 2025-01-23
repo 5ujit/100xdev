8953befd755e1b5bd37a15957d3bca4a4d4e890b
5ujit <sksujit021@gmail.com>
2025-01-23 23:28:56 +0530

    Create Scope.java

---
+++ 100xDSA/src/function/Scope.java
@@ -0,0 +1,24 @@
+package function;
+
+public class Scope {
+    public static void main(String[] args) {
+        int a= 10;
+        int b=20;
+
+
+
+        {
+            a=5;// can reasin the value what we made outside of the scope
+//            int a= 5;  can't do this
+            int c= 20;
+
+        }
+        int c= 30;// see here we can aingn the value of c outside of the scope of
+
+    }
+    static void Gopi(){
+
+    }
+}
+/*
+ */
