e693078cdddd26074b344ee62ff352c4b7971f51
5ujit <sksujit021@gmail.com>
2025-02-04 23:53:01 +0530

    Create Cdoe6.java

---
+++ 100xDSA/src/Basics_Qestion_for_logics/Cdoe6.java
@@ -0,0 +1,15 @@
+package Basics_Qestion_for_logics;
+
+import java.util.Scanner;
+
+public class Cdoe6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a= sc.nextInt();
+        System.out.println("enter a num ");
+        int count=0;
+
+
+    }
+}
