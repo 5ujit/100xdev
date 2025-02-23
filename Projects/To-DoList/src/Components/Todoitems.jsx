ab13aba811d672822ec2024fc085e19e78e4e838
5ujit <sksujit021@gmail.com>
2025-02-23 18:53:14 +0530

    To-do app with React

--- Projects/To-DoList/src/Components/Todoitems.jsx
+++ Projects/To-DoList/src/Components/Todoitems.jsx
@@ -3,19 +3,20 @@ import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
 
-const Todoitems = () => {
+const Todoitems = ({text,id,isComplete: isComplete,deleteTodo,toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-        <div className='flex flex-1 items-center curser-pointer'>
-            <img src={tick} className='w-7' alt="" />
+        <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
+            {/* <img src={ isComplete? tick:not_tick} className='w-7 cursor-pointer' alt="" /> */}
 
+            <img src={isComplete? tick:not_tick}   alt="" className='w-7' />
 
-
-
-
-            <p className='text-slate-700 ml-4 text-[17]'>Learn coding  </p>
+            <p className= {  `tex-slate-700 ml-4 text-[17px] decoration-slate-500
+              ${isComplete ? "line-through" : ""} `}> 
+                {text}  </p>
         </div>
-        <img src={delete_icon} alt="" />
+
+        <img onClick={()=>{deleteTodo(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer' />
     </div>
   )
 }
