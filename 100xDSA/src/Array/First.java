e7aa7f46712938ec834432bfd3df0acbfe6e3285
5ujit <sksujit021@gmail.com>
2025-02-26 23:47:26 +0530

    java question

---
+++ 100xDSA/src/Array/First.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello this is array");
+
+        // int[] numbers;
+        // numbers= new int[5];
+
+        int[]numbers= new int[5];
+
+        //🚫 int[] numbers = { 10, 20, 30, 40, 50, }; // Declare + Initialize in one step
+        numbers= new int[]{10,34,54,54,43,222,12};
+
+
+        System.out.println(numbers[4]); // Output: 50
+        System.out.println(numbers[6]);
+    }
+
+}
---
+++ 100xDSA/src/Basics_Qestion_for_logics/Tax.java
@@ -0,0 +1,8 @@
+package Basics_Qestion_for_logics;
+
+public class Tax {
+    public static void main(String[] args) {
+        System.out.println("tax question ");
+        
+    }
+}
