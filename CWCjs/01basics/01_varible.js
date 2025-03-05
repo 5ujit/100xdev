8e7a0bfbb61de230efeb93c4ad74c6c51a7f1b62
5ujit <sksujit021@gmail.com>
2025-03-05 23:26:08 +0530

    .

---
+++ CWCjs/01basics/01_varible.js
@@ -0,0 +1,21 @@
+const accoutntId=12;
+let accountEmail= "sujit.@google.com"
+var accountPassword= "341241";
+
+accountCity="jaipur"
+
+// accoutntId=12313
+
+acoutnEmail="hc@hc.com"
+accountPassword= "2121212"
+
+accountCity= "Bengalured"
+
+console.table([accoutntId,accountEmail,accountPassword,accountCity])
+
+
+/**
+ * prefer not to use var
+ * bz of issue in block scope and funcitonal s
+ */
+
