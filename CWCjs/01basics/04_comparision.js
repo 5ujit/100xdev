dbf03ae4104f2cfb915f0999f8ca2eaf27351475
5ujit <sksujit021@gmail.com>
2025-03-08 16:05:17 +0530

    data type summary

--- CWCjs/01basics/04_comparision.js
+++ CWCjs/01basics/04_comparision.js
@@ -1,8 +1,14 @@
-console.log(2>1);
-console.log(2>=1);
-console.log(2<1);
-console.log(2==1);
-console.log(2!=1);
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
 
-console.log("2">1);
-console.log("2">1);
+// console.log("2">1);
+// console.log("2">1);
+
+console.log("2"> 1);
+console.log("02">1);
+
+
+
