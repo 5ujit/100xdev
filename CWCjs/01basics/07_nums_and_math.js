3865bc19f459b8b4afd1016adeca72c29b27a830
5ujit <sksujit021@gmail.com>
2025-03-09 22:42:11 +0530

    number and math in js

---
+++ CWCjs/01basics/07_nums_and_math.js
@@ -0,0 +1,47 @@
+const score =400
+// console.log(score);
+
+
+const balance =new Number(100)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const otherNumber= 123.8966
+
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds=10000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString());
+
+
+// console.log(Intl.NumberFormat().resolvedOptions().locale);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++Math👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻Math👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6));
+
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(3,3,342,5,5,6,22,4));
+// console.log(Math.max(3,3,342,5,5,6,22,4));
+
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10;
+const max =20;
+
+console.log(Math.random()*(max));
+ 
+console.log(Math.floor(Math.random()*(max-min+1)) +min);
