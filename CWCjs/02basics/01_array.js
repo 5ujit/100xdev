ef598b0c108c686ef4f27c3749e87ceedf428ad4
5ujit <sksujit021@gmail.com>
2025-03-11 23:48:37 +0530

    basics part 2

---
+++ CWCjs/02basics/01_array.js
@@ -0,0 +1,11 @@
+const myArr =[0,1,2,3,4,5]
+// const myHeors=["shaktiman","naagraj"];
+
+const myArr2=new Array(1,2,3,4,5)
+// console.log(myArr[1]);
+
+// console.log(myArr2);
+
+
+myArr.push(6)
+console.log(myArr);
