5e87f88ea73dd6e218ccf55c7688e9296073d126
5ujit <sksujit021@gmail.com>
2025-03-14 23:45:30 +0530

    array part 2

--- CWCjs/02basics/01_array.js
+++ CWCjs/02basics/01_array.js
@@ -35,5 +35,8 @@ console.log("B ", myArr);
 
 
 const myn2 = myArr.splice(1, 3)
+
 console.log("C ", myArr);
+
 console.log(myn2);
+
---
+++ CWCjs/02basics/02_array.js
@@ -0,0 +1,14 @@
+console.log(Array.prototype);
+
+// push() vs concat( ) 
+
+let arr1 = [1,2,3];
+arr1.push(4,5);
+console.log(arr1);
+
+let arr2 = [1,2,3];
+
+
+let newArr = arr2.concat([5,6])
+
+console.log(newArr);
