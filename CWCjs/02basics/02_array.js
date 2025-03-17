45774b742dae439b382862ced7ca8834a80ac430
5ujit <sksujit021@gmail.com>
2025-03-17 23:53:04 +0530

    js array part 2

--- CWCjs/02basics/02_array.js
+++ CWCjs/02basics/02_array.js
@@ -1,14 +1,3 @@
-console.log(Array.prototype);
-
-// push() vs concat( ) 
-
-let arr1 = [1,2,3];
-arr1.push(4,5);
-console.log(arr1);
-
-let arr2 = [1,2,3];
-
-
-let newArr = arr2.concat([5,6])
-
-console.log(newArr);
+const marvel_hero = ["thor", "Ironman","spiderman"]
+const dc_heros=["superman","flash", "batman"]
+marval_hea
