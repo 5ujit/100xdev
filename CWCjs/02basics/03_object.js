dd4b7c494d9f523f2c7cb80a86250e1427f6e4db
5ujit <sksujit021@gmail.com>
2025-03-23 23:59:31 +0530

    .

--- CWCjs/02basics/03_object.js
+++ CWCjs/02basics/03_object.js
@@ -3,36 +3,45 @@
 
 // object literals
 
-const mySym = Symbol("key1")
+const mySym = Symbol(123);
 
+const mySym1 = Symbol(1);
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
-    age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
-}
+  name: "sujit",
+  "full name": "Hitesh Choudhary",
+  [mySym]: "key1",
+  age: 18,
+  location: "Jaipur",
+  email: "sujit@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+// console.log(JsUser["full name"]);
+console.log(typeof JsUser[mySym]);
 
-JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
-}
+console.log(JsUser[mySym]);
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// // console.log(JsUser.email)
+// // console.log(JsUser["email"])
+// // console.log(JsUser["full name"])
+// // console.log(JsUser[mySym])
+
+// JsUser.email = "hitesh@chatgpt.com"
+// // Object.freeze(JsUser)
+// JsUser.email = "hitesh@microsoft.com"
+// // console.log(JsUser);
+
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
+
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
