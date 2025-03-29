d254b871d25fc2823903a556cf1995c3ecad580f
5ujit <sksujit021@gmail.com>
2025-03-29 22:43:36 +0530

    Create TypeConversion.java

---
+++ 100xDSA/src/Java_310_Etp/TypeConversion.java
@@ -0,0 +1,22 @@
+package Java_310_Etp;
+
+public class TypeConversion {
+    public static void main(String[] args){
+
+//        int num =100;
+//        double d= num ;
+//        System.out.println(d);
+
+
+        double d= 9.99;
+        int sum = (int)d;
+        System.out.println(d);
+        System.out.println(sum);
+
+        char ch='a';
+        int b=(int)ch;
+        System.out.println(b);
+
+
+    }
+}
