1d4ce9b17fe8c04f85ebba704d2c98006e6be8f6
5ujit <sksujit021@gmail.com>
2025-04-03 23:57:46 +0530

    b

--- CWCjs/03basics/01_function.js
+++ CWCjs/03basics/01_function.js
@@ -1,6 +1,62 @@
 function addTwoNumber(number1,number2){//<-- parameters 
-    console.log(number1+number2);
+    // let result =number1+number2
+    // return result
+
+    return number1+number2
+}
+
+//<-- calling the function
+const result= addTwoNumber(3,5)// <-- arguments..
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "sam"){
+    if(!username ){
+        console.log("please enter a username");
+        return
+    }
+    return `${username} just logged in `
+}
+
+
+
+
+// console.log(loginUserMessage());//undefined 
+// console.log(loginUserMessage("Hitesh"));
+
+
+
+
+function calcualteCartPrice(a,b,...num1){
+    return num1 
+}
+
+
+// console.log(calcualteCartPrice(200,400,500,2000));
+
+
+const user ={
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject( anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price} `);
     
 }
-const result= addTwoNumber(3,5)// <-- arguments..
-console.log("Result: ", result);
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondVlaue(getArray){
+    return getArray[1]
+}
+
+
+
+// console.log(returnSecondVlaue(myNewArray));
---
+++ CWCjs/03basics/02_scopes.js
@@ -0,0 +1,59 @@
+var c=30;
+let a= 300;
+if (true){
+    let a = 10;
+    const b= 20;
+    // console.log("INNER SCOPE:" , a);
+    
+}
+
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one (){
+    const username = "hitesh"
+    
+    function two (){
+        const website ="youtuve"
+        // console.log(username);
+    }
+    // console.log(website);
+    // two()
+    
+}
+// one();
+
+
+if ( true){
+    const username ="hitesh"
+    if(username ==="histesh"){
+        const website =" youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+
+}
+// console.log(username);
+
+
+//++++++++++++++++++++++++++++++++++++++++++ interersting +++++++++++++++++++++
+
+function addOne(num){
+    return num +1
+}
+
+addOne(5)
+
+
+const addTwo= function(num){
+    return num +2
+}
+
+addTwo(5)
+const addTwo = 
