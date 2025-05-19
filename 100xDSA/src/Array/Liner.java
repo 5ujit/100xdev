78e90312030f1ef814524f15da4836aa68b8fb0c
5ujit <sksujit021@gmail.com>
2025-05-19 23:57:07 +0530

    Create Liner.java

---
+++ 100xDSA/src/Array/Liner.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Liner {
+    public static void main(String[] args) {
+      int[] arr={1,2,3,4,55,};
+      for (int i =0; i< arr.length; i++){
+          if()
+      }
+        System.out.println();
+    }
+}
