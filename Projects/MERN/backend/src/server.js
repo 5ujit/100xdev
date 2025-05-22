bbff8169f5e1556e1a8e19766db82368b5b5cd07
5ujit <sksujit021@gmail.com>
2025-05-22 23:50:50 +0530

    middleware

--- Projects/MERN/backend/src/server.js
+++ Projects/MERN/backend/src/server.js
@@ -14,7 +14,14 @@ const PORT= process.env.PORT || 5001
 connectDB();
 
 //middleware
-app.use(express.json())
+app.use(express.json())// this middleware will parse JSON  bodies req.body 
+/*
+our simple custom middleware
+app.se((req,res,next)=>{
+    console.log(`Req method is ${req.method} & Req URL  is ${req.url}`);
+    next();
+    })
+*/
 app.use("/api/notes", notesRoutes);
 
 app.listen(PORT,()=>{
