d622f7f58bf005292e02adfbbb06d0778e38713f
5ujit <sksujit021@gmail.com>
2025-05-24 23:35:34 +0530

    Appling frontend

---
+++ 100xDSA/src/Array/LinerSearch/Q_01.java
@@ -0,0 +1,15 @@
+package Array.LinerSearch;
+
+public class Q_01 {
+    public static void main(String[] args) {
+
+    }
+    // search in the arrary : return the index if item found
+
+
+    static int linerSerch(int[] arr, int target) {
+        if (arr.length == 0) {
+        }
+    }
+
+}
