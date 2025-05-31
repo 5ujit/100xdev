d67eca0f1418558b1fc50622b505f5f96b3ae7f2
5ujit <sksujit021@gmail.com>
2025-05-31 23:03:05 +0530

    /

--- Projects/MERN/frontend/src/App.jsx
+++ Projects/MERN/frontend/src/App.jsx
@@ -5,10 +5,11 @@ import CreatePage from './pages/CreatePage';
 import NoteDetailpage from './pages/NoteDetailpage'
 
 
+
 const App = () => {
   return (
     <div className="relative h-full w-full" data="forest">
-
+      
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
   
       <Routes>
--- Projects/MERN/frontend/src/pages/NoteDetailpage.jsx
+++ Projects/MERN/frontend/src/pages/NoteDetailpage.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
 
 const NoteDetailpage = () => {
+  const [note,setNote]= useState(null)
+  const [loading,setLoading]=useState(true)
+  const [saving ,setSaving]=useState(false)
+
+  const navigage= useNavigate()
+  const {id} = useParams();
+
+  console.log({id});
+  
   return (
     <div>
       
