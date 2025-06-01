7457ba4f73dc8abdfe7c0b99a2ee998b23d2be00
5ujit <sksujit021@gmail.com>
2025-06-01 23:29:12 +0530

    card details page

--- Projects/MERN/frontend/src/pages/NoteDetailpage.jsx
+++ Projects/MERN/frontend/src/pages/NoteDetailpage.jsx
@@ -1,21 +1,36 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router'
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate, useParams } from "react-router";
+import api from "../lib/axios.js";
 
 const NoteDetailpage = () => {
-  const [note,setNote]= useState(null)
-  const [loading,setLoading]=useState(true)
-  const [saving ,setSaving]=useState(false)
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
 
-  const navigage= useNavigate()
-  const {id} = useParams();
+  const navigage = useNavigate();
 
-  console.log({id});
+  const { id } = useParams();
+  // console.log({  id});
+  // console.log( "id is here ", id);
   
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`/notes/${id}`);
+        setNote(res.data);
+      } catch (error) {
+        console.log("Error in fethching note ", error);
+        toast.error("Faild to fetch the note ");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNote();
+  }, [id]);
+  console.log({ note });
 
-export default NoteDetailpage
+  return <div>Note Details</div>;
+};
+
+export default NoteDetailpage;
